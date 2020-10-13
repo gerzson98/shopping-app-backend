b6c03e5060f2526a1d5c7044383870265d08ef43
Bence Márkus <markusbence@outlook.com>
2020-10-13 19:23:56 +0200

    db configuration

--- server/mongoDB.js
+++ server/mongoDB.js
@@ -1,13 +1,12 @@
 'use strict'
 
 const mongoose = require('mongoose')
-
-
+const { database } = require('../config/config')
 /**
  * @description creates connection with database
  */
 const connectToDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(database.mongoURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { server } = require('../config/config')
+const connectToDB = require('./mongoDB')
 const { attachRoutes } = require('../api/attachRoutes')
 
 const express = require('express')
@@ -11,8 +12,14 @@ const app = express()
  * todo(Bence): after config created normally we need to replace it with async
  */
 
-const start = function (app) {
+const start = async function (app) {
   const port = server.port
+  try {
+    await connectToDB()
+  } catch (error) {
+    console.log(error)
+  }
+  app.use(express.json())
   attachRoutes(app).listen(port, () => {
     console.info(`Server running on ${port}`)
   })
